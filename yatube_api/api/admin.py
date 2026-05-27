fc1ce8b36938af2db0a1c49048d7378543b159cf
devpankov <mikhailpankov166@gmail.com.email>
2026-05-27 23:34:55 +0300

    Fix flake8 configuration

--- yatube_api/api/admin.py
+++ yatube_api/api/admin.py
@@ -1,3 +1,1 @@
-from django.contrib import admin
-
-# Register your models here.
+# Admin registrations are not required for the API app.
--- yatube_api/posts/views.py
+++ yatube_api/posts/views.py
@@ -1,3 +1,1 @@
-from django.shortcuts import render
-
-# Create your views here.
+# Views for posts are implemented in the API app.
